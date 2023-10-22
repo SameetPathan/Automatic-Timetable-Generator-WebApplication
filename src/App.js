166a06eb38d31f22bc5491506a00e20c93953dbc
SameetPathan <sameetpathanrs@gmail.com>
2023-10-23 00:27:11 +0530

    v1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import NavbarComponent from './components/NavbarComponent';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <NavbarComponent></NavbarComponent>
+      <header className="App-header text-center">
+        <h1>Welcome to the Automatic Timetable Generator</h1>
+        <p className="lead">
+          Generate your timetable effortlessly with our advanced generator.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
 }
+
 
 export default App;
